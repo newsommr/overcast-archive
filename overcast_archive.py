d9dc1fc0711d6b12f4440f7d77c815b54353378b
newsommr <79298192+newsommr@users.noreply.github.com>
2023-05-28 13:32:52 -0400

    Add 'all' functionality
    Instead of listing the podcasts, you can just type in 'all' to download them all

--- overcast_archive.py
+++ overcast_archive.py
@@ -20,43 +20,40 @@ def download_podcasts(file):
         print(f"Failed to parse the file '{file}' as XML. Error: {e}")
         return
 
-    # Find all the main 'outline' elements with type attribute 'rss' that represent individual podcasts
     podcasts = bs_data.find_all('outline', {'type': 'rss'})
-
-    # Get a list of podcast names
     podcast_names = [podcast['title'] for podcast in podcasts]
 
-    # Ask the user to select which podcasts to download
     print("Available podcasts:")
     for i, name in enumerate(podcast_names, start=1):
         print(f"{i}. {name}")
 
-    selected_podcasts = input("Enter the numbers of the podcasts you want to download, separated by commas: ")
-    try:
-        selected_podcasts = [int(num.strip()) for num in selected_podcasts.split(",")]
-    except ValueError:
-        print("Invalid input. Please enter numbers separated by commas.")
-        return
+    selected_podcasts = input("Enter the numbers of the podcasts you want to download, separated by commas, or 'all' to download all: ")
+
+    if selected_podcasts.lower() == 'all':
+        selected_podcasts = list(range(1, len(podcast_names) + 1))
+    else:
+        try:
+            selected_podcasts = [int(num.strip()) for num in selected_podcasts.split(",")]
+        except ValueError:
+            print("Invalid input. Please enter numbers separated by commas, or 'all' to download all.")
+            return
 
     for i in selected_podcasts:
         try:
-            podcast = podcasts[i-1]  # list indices start at 0, but we displayed the list starting at 1
+            podcast = podcasts[i-1]
             podcast_name = podcast_names[i-1]
         except IndexError:
             print(f"No podcast found with the number {i}.")
             continue
 
         podcast_dir = os.path.join('podcasts', podcast_name)
-
-        # Create a directory for the podcast if it doesn't exist
         if not os.path.exists(podcast_dir):
             os.makedirs(podcast_dir)
 
-        # Find all the 'outline' elements within the current podcast that represent individual episodes
         episodes = podcast.find_all('outline')
 
         for episode in episodes:
-            if 'played' in episode.attrs and episode['played'] == '1':  # The episode has been played
+            if 'played' in episode.attrs and episode['played'] == '1':
                 mp3_url = episode.get('enclosureUrl', None)
                 if mp3_url is None:
                     print(f"No URL found for the episode '{episode.get('title', '')}'. Skipping.")
@@ -64,7 +61,7 @@ def download_podcasts(file):
 
                 mp3_file = os.path.join(podcast_dir, f"{episode['title']}.mp3")
 
-                if not os.path.exists(mp3_file):  # Check if the file already exists
+                if not os.path.exists(mp3_file):
                     print(f"Downloading {episode['title']}...")
                     try:
                         mp3_data = requests.get(mp3_url).content
@@ -81,5 +78,4 @@ def download_podcasts(file):
                 else:
                     print(f"{episode['title']} already exists. Skipping download.")
 
-# Call the function
 download_podcasts('overcast.opml')
